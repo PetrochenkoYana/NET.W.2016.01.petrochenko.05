1c2f1f991f921f435304730c72fb9a414d0977dd
PetrochenkoYana <petrochenko.yana@gmail.com>
2016-10-31 11:59:21 +0300

    First commit

---
+++ Task1/EuclidsGCD.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Task1
+{
+   /// <summary>
+   /// Class-Wrapper for containing static members
+   /// </summary>
+    public static class EuclidsGcd
+
+    {
+        /// <summary>
+        /// Method for searching the greatest common divisor with an algorithm created by Euclid
+        /// </summary>
+        /// <param name="time">timeSpan variable for getting duration of method</param>
+        /// <param name="a">First number </param>
+        /// <param name="b">Second number</param>
+        /// <returns>GCD</returns>
+        public static int GCD( out TimeSpan time, int a, int b)
+        {
+            Stopwatch stopwatch1 = new Stopwatch();
+            stopwatch1.Restart();
+            while (b != 0)
+            {
+                int tmp = b;
+                b = a%b;
+                a = tmp;
+            }
+            stopwatch1.Stop();
+            time=stopwatch1.Elapsed;
+            return Math.Abs(a);
+            
+
+        }
+        /// <summary>
+        /// The same method for searching GCD, but for more then 2 numbers
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static int GCD( out TimeSpan time, params int[] numbers)
+        {
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch2.Restart();
+            int i = 1;
+            int res=GCD( out time,numbers[0],numbers[1]);
+            while (i < numbers.Length-1)
+            {
+                 res =  GCD( out time,res, numbers[i + 1]);
+                i++;
+            }
+            stopwatch2.Stop();
+            time = stopwatch2.Elapsed;
+            return Math.Abs(res);
+        }
+    }
+}
---
+++ Task1Tests/Tests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task1;
+namespace Task1Tests
+{
+    [TestFixture]
+    class Tests
+    {
+        TimeSpan time = new TimeSpan();
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.AreEqual(21,EuclidsGcd.GCD(out time,1071, 462));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+            
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(21, EuclidsGcd.GCD(out time, 462, 1071));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(21, EuclidsGcd.GCD(out time, 462, 1071,21));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(21, EuclidsGcd.GCD(out time, 462, 1071,0));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Assert.AreEqual(1, EuclidsGcd.GCD(out time, 462, 1071,467));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+        }
+
+        [Test]
+        public void Test6()
+        {
+            Assert.AreEqual(1, EuclidsGcd.GCD(out time, 462, 1071, 21,1,5,7));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+        }
+
+        [Test]
+        public void Test7()
+        {
+            Assert.AreEqual(1, EuclidsGcd.GCD(out time, 462, 1071, 467,0,0));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+        }
+
+        [Test]
+        public void Test8()
+        {
+            Assert.AreEqual(7, EuclidsGcd.GCD(out time, 462, 1071,42,84,7));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+        }
+
+        [Test]
+        public void Test9()
+        {
+            Assert.AreEqual(3, EuclidsGcd.GCD(out time, 462, 1071,3,3,3,3,3,3,3,3,21));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+        }
+
+        [Test]
+        public void Test10()
+        {
+            Assert.AreEqual(1, EuclidsGcd.GCD(out time, 462, 1071, 467,0,0,0,0,0,0,5));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+        }
+        [Test]
+        public void Test11()
+        {
+            Assert.AreEqual(1, EuclidsGcd.GCD(out time, -462, 1071, 467, 0, 0, 0, 0, 0, 0, 5));
+            Console.WriteLine(time.TotalMilliseconds.ToString());
+        }
+
+        /// <summary>
+        /// tests 
+        /// </summary>
+        [Test]
+        public void Test12()
+        {
+            Assert.AreEqual(21, BinaryGCD.GCD(462,1071));
+           
+        }
+        [Test]
+        public void Test13()
+        {
+            Assert.AreEqual(21, BinaryGCD.GCD(462, 1071,0));
+
+        }
+        [Test]
+        public void Test14()
+        {
+            Assert.AreEqual(3, BinaryGCD.GCD(462, 1071, 3, 3, 3, 3, 3, 3, 3, 3, 21));
+
+        }
+        [Test]
+        public void Test15()
+        {
+            Assert.AreEqual(1, BinaryGCD.GCD(-462, 1071, 467, 0, 0, 0, 0, 0, 0, 5));
+
+        }
+
+    }
+}
