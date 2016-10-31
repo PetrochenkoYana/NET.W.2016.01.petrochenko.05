d4e8f62f8c492df4bd71b5e329ebc3c46295934b
PetrochenkoYana <petrochenko.yana@gmail.com>
2016-10-31 17:47:05 +0300

    Second commit

---
+++ Task2/MatrixFunc.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class MatrixFunc
+    {
+
+       
+       
+        public static void BubbleSortSumUp( int[][] matrix)
+        {
+            int[] arraySum = ArraySumStringMatrix(matrix);
+            for (int i = 0; i < arraySum.Length; i++)
+            {
+                for (int j = 0; j < arraySum.Length-i-1; j++)
+                {
+                    if (arraySum[j] > arraySum[j + 1])
+                    {
+                        var temp = arraySum[j];
+                        arraySum[j] = arraySum[j + 1];
+                        arraySum[j + 1] = temp;
+                        var tmp = matrix[j + 1];
+                        matrix[j + 1] = matrix[j];
+                        matrix[j] = tmp;
+                    }
+                }
+
+            }
+
+        }
+        /// <summary>
+        /// Method for counting sums by strings in matrix
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns>Return array, sums for strings in matrix</returns>
+        private static int[] ArraySumStringMatrix(int[][] matrix)
+        {
+           int[] array=new int[matrix.Length];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                array[i] = ArraySum(matrix[i]);
+            }
+            return array;
+
+        }
+        /// <summary>
+        /// Sum of array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        private static int ArraySum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+                        sum += array[i];
+            return sum;
+
+        }
+    }
+}
---
+++ Task2Tests/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2;
+using NUnit.Framework;
+
+namespace Task2Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            int[][] array = new int[][] {new []{1, 2, 3, 4},new []{2, 3, 4},new [] {4, 5, 6, 7, 432, 4}, new[] { 5, 5, 55555 }, new[] { 0, 5, 6, 7, 0, 4 } };
+           MatrixFunc.BubbleSortSumUp(array);
+
+        }
+    }
+}
